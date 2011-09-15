ac6be211a3032f8f5e1f3e2646964088cdbfa264
carltape <carltape@9a5ef318-cf23-c2a7-89b0-81a7d1a0783c>
2011-09-15 18:08:21 +0000

    modified rotmat.m to allow for input vector

--- trunk/surfacevel2strain/matlab/util_basic/randomvec.m
+++ trunk/surfacevel2strain/matlab/util_basic/randomvec.m
@@ -3,9 +3,13 @@
 %
 % This generates a vector of n random numbers in the range [xmin,xmax].
 %
+% Is there a matlab built-in function that does this?
+% There is randi for integers, but what about for non-integers?
+%
 
 function x = randomvec(xmin0, xmax0, n)
 
+% flip input order if needed
 xmin = min([xmin0 xmax0]);
 xmax = max([xmin0 xmax0]);
 
