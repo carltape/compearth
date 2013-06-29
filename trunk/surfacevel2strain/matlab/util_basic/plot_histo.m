5adb8f3bf140f0648be0341fcc470010363db6d3
carltape <carltape@9a5ef318-cf23-c2a7-89b0-81a7d1a0783c>
2013-06-29 18:22:05 +0000

    changed optional input arguments for rotate_points_MT.m

--- trunk/surfacevel2strain/matlab/util_basic/plot_histo.m
+++ trunk/surfacevel2strain/matlab/util_basic/plot_histo.m
@@ -1,12 +1,19 @@
-function N = plot_histo(hdat,edges)
+function [N,Nplot] = plot_histo(hdat,edges,itype)
 %PLOT_HISTO plot a histogram with cyan bars and black boundaries
 
+if nargin==2, itype=2; end
+
+dbin = edges(2) - edges(1);
 Ntotal = length(hdat);
 [N,bin] = histc(hdat,edges);
-bar(edges,N/Ntotal,'histc');
+switch itype
+    case 1, Nplot = N; xlab = 'Count';
+    case 2, Nplot = N/Ntotal; xlab = 'Fraction';
+    case 3, Nplot = N/Ntotal/dbin; xlab = 'PDF';
+end
+bar(edges,Nplot,'histc');
 xlim([min(edges) max(edges)]);
-ylabel(sprintf('p(x) (N=%i)',Ntotal));
-%ylabel(['Fraction (N = ' num2str(Ntotal) ')']);
+ylabel(sprintf('%s (N=%i)',xlab,Ntotal));
 
 h = findobj(gca,'Type','patch');
 set(h,'FaceColor',[0 1 1],'EdgeColor','k');
