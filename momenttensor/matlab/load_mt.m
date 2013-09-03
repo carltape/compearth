ae61e8cc66f2ca9119dc266d449dedeb828d6cea
Carl Tape <carltape@gmail.com>
2013-09-03 17:44:39 +0000

    modified instructions for using moment tensor scripts

--- momenttensor/matlab/load_mt.m
+++ momenttensor/matlab/load_mt.m
@@ -1,6 +1,13 @@
+%
 % load_mt.m
 %
 % Set paths to additional directories in compearth
+% (These are needed for utilities such as rotation scripts.)
 
-addpath('../../surfacevel2strain/matlab/util_basic');
-addpath('../../surfacevel2strain/matlab/util_euler');
+% path to the compearth head directory 
+%compearth = strcat(getenv('HOME'),'/compearth/');  % absolute path (safest)
+compearth = '../../';                               % relative path
+
+addpath(strcat(compearth,'surfacevel2strain/matlab/util_basic'));
+addpath(strcat(compearth,'surfacevel2strain/matlab/util_euler'));
+addpath(strcat(compearth,'momenttensor/matlab'));
