af056b153301190c3275d58cb2a994692a367f6e
Carl Tape <carltape@gmail.com>
2013-12-18 23:33:25 +0000

    added functions to convert between eigenvalues and nu-alpha parameters

---
+++ momenttensor/matlab/lam2nualpha.m
@@ -0,0 +1,28 @@
+function [nu,alpha] = lam2nualpha(lam)
+%LAM2NUALPHA converts eigenvalues to nu and alpha
+%
+% INPUT
+%   lam     3 x n set of eigenvalue triples
+%
+% OUTPUT
+%   nu      n x 1 vector of Poisson parameters (unitless)
+%   alpha   n x 1 vector of angles between fault normal and slip vector, degrees
+%
+% Reverse function is nualpha2lam.m
+% See Tape and Tape (2013), "The classical model for moment tensors"
+% 
+% Carl Tape, 08-Jan-2013
+%
+
+lam = lamsort(lam);
+
+lam1 = lam(1,:);
+lam2 = lam(2,:);
+lam3 = lam(3,:);
+
+% TT2013, Eqs 32ab
+alpha = 180/pi* acos( (lam1 - lam2 + lam3) ./ (lam1 - lam3) );
+nu = lam2 ./ (lam1 + lam3);
+
+alpha = alpha(:);
+nu = nu(:);
---
+++ momenttensor/matlab/lamsort.m
@@ -0,0 +1,9 @@
+function lam = lamsort(lam)
+%LAMSORT sort eigenvalues as lam1 >= lam2 >= lam3
+
+[a,n] = size(lam);
+if a~=3, error('dimension of lam (%i x %i) must be 3 x %i',a,n,n); end
+if n==3, disp('warning: lam is 3 x 3, so make sure that each column corresponds to a lambda vector'); end
+
+% our formulas assume that eigenvalues are sorted as lam1 >= lam2 >= lam3
+lam = sort(lam,'descend');
