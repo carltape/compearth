8b8d7ffc8cd6d3ffac69a478f00e063ed87f3678
Carl Tape <carltape@gmail.com>
2014-07-10 05:40:30 +0000

    comments

--- surfacevel2strain/matlab/util_basic/cpt2cmap.m
+++ surfacevel2strain/matlab/util_basic/cpt2cmap.m
@@ -1,16 +1,6 @@
-%
-% function outmat = cpt2cmap(inmat)
-%
-% This function inputs a 3 column matrix of colors (RGB)
-% and outputs a matrix suitable for Matlab plots.
-%
-% calls xxx
-% called by colors.m
-%
-
 function outmat = cpt2cmap(inmat)
+%CPT2CMAP input a nx3 matrix of RGB colors and output a matrix suitable for colormap
 
-numc = 65;
 len = length(inmat);
 temp = inmat/max(max(inmat));  % normalize to 0-1 range
 rv = temp(:,1);
@@ -22,5 +12,3 @@ numc = 65;
 outmat = [  interp1(linspace(1,numc,len), rv', [1:numc])' ...
             interp1(linspace(1,numc,len), gv', [1:numc])' ...
             interp1(linspace(1,numc,len), bv', [1:numc])'];
-
-%==================================================
