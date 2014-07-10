e316b9a73a695deeff028819e4f75657965b3728
Carl Tape <carltape@gmail.com>
2014-07-10 05:40:16 +0000

    option to return binned values but not plot

--- surfacevel2strain/matlab/util_basic/plot_histo.m
+++ surfacevel2strain/matlab/util_basic/plot_histo.m
@@ -1,9 +1,14 @@
 function [N,Nplot,centers] = plot_histo(hdat,edges,itype)
 %PLOT_HISTO plot a histogram with cyan bars and black boundaries
 
+make_plot = true;
+
+% default is to plot the fraction of the total for each bin
 if nargin==2, itype=2; end
 
+% bin width (only relevant if bins are the same width)
 dbin = edges(2) - edges(1);
+
 Ntotal = length(hdat);
 [N,bin] = histc(hdat,edges);
 switch itype
@@ -11,18 +16,21 @@ switch itype
     case 2, Nplot = N/Ntotal; xlab = 'Fraction';
     case 3, Nplot = N/Ntotal/dbin; xlab = 'PDF';
 end
-bar(edges,Nplot,'histc');
-xlim([min(edges) max(edges)]);
-ylabel(sprintf('%s (N=%i)',xlab,Ntotal));
 
-h = findobj(gca,'Type','patch');
-set(h,'FaceColor',[0 1 1],'EdgeColor','k');
+if make_plot
+    bar(edges,Nplot,'histc');
+    xlim([min(edges) max(edges)]);
+    ylabel(sprintf('%s (N=%i)',xlab,Ntotal));
 
-if length(hdat) ~= sum(N)
-   disp(sprintf('NOTE (plot_histo.m): You may want to extend the edges --'));
-   disp(sprintf(' there are %i/%i input that are outside the specified range',...
-       length(hdat)-sum(N),length(hdat)));
-   %disp(sprintf(' the number of input (%i) does not equal the sum of bin counts (%i).',length(hdat),sum(N)));
+    h = findobj(gca,'Type','patch');
+    set(h,'FaceColor',[0 1 1],'EdgeColor','k');
+
+    if length(hdat) ~= sum(N)
+       warning('(plot_histo.m): You may want to extend the histogram edges -->');
+       disp(sprintf(' there are %i/%i input that are outside the specified range',...
+           length(hdat)-sum(N),length(hdat)));
+       %disp(sprintf(' the number of input (%i) does not equal the sum of bin counts (%i).',length(hdat),sum(N)));
+    end
 end
 
 centers = edges + dbin/2;
@@ -31,3 +39,10 @@ centers(end) = [];
 % (see histc), so we cut them
 N(end) = [];
 Nplot(end) = [];
+
+% if the bin widths are different sizes, then matlab plots asterix symbols
+% at the base of the bins (WHY?), so here we delete them
+if length(unique(edges)) > 1
+    sh=findall(gcf,'marker','*');
+    delete(sh);
+end
