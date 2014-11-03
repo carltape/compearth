993b2385f4e7fd5c8dcde30229d9dae07d8873f8
carltape <carltape@9a5ef318-cf23-c2a7-89b0-81a7d1a0783c>
2014-11-03 22:53:02 +0000

    add script to plot two histograms superimposed

---
+++ trunk/surfacevel2strain/matlab/util_basic/plot_histo_pair.m
@@ -0,0 +1,19 @@
+function plot_histo_pair(hdat1,hdat2,st1,st2,edges)
+%PLOT_HISTO_PAIR plots two dets of data as a superimposed histogram
+
+figure; hold on;
+
+Ntotal = length(hdat1);
+[N,bin] = histc(hdat1,edges);
+bar(edges,N/Ntotal,'histc');
+h = findobj(gca,'Type','patch'); set(h,'FaceColor',[0 1 1],'EdgeColor','k');
+
+Ntotal = length(hdat2);
+[N,bin] = histc(hdat2,edges);
+j = bar(edges,N/Ntotal,'histc');
+set(j,'FaceColor','none','EdgeColor','r','linewidth',3);
+xlim([min(edges) max(edges)]);
+ylabel('Fraction of total');
+legend(sprintf('%s (N = %i)',st1,length(hdat1)),...
+    sprintf('%s (N = %i)',st2,length(hdat2)));
+% note: need to add xlabel
