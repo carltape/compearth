2ac5be0963938ccdf4afe5287717e9fb97c2d8fe
Carl Tape <carltape@gmail.com>
2015-02-21 01:38:05 +0000

    change to omegadc

--- momenttensor/matlab/CMT2omegadc_xi0.m
+++ momenttensor/matlab/CMT2omegadc_xi0.m
@@ -1,5 +1,5 @@
-function [omega,xi0,U] = CMT2omega_xi0(X1,X2,iorthoU,idisplay)
-%CMT2OMEGA_XI0 compute the omega and xi0 angles between two moment tensors
+function [omegadc,xi0,U] = CMT2omegadc_xi0(X1,X2,iorthoU,idisplay)
+%CMT2OMEGADC_XI0 compute the omegadc and xi0 angles between two moment tensors
 %
 % INPUT
 %   X1,X2     can be either U1,U2 or M1,M2 (see below)
@@ -11,11 +11,13 @@ function [omega,xi0,U] = CMT2omega_xi0(X1,X2,iorthoU,idisplay)
 %   U1,U2     3 x 3 x n bases
 %
 % OUTPUT
-%   omega     9-angle between closest double couples
+%   omegadc     9-angle between closest double couples
 %   xi0       minimum rotation angle between principal axes
 %   U         3 x 3 x n rotation matrix U = U1' * U2
 %
 % For details, see TapeTape2012 "Angle between principal axis triples".
+% The omega angle in TapeTape2012 is what we denote as omegadc here;
+% we use omega to represent the angle between two moment tensors (see CMT2omega.m).
 %
 % EXAMPLES: see below and also TT2012kaganAppE.m
 % Set bfigure=true to plot histograms of the distributions.
@@ -28,7 +30,7 @@ bfigure = true;
 if ~exist('idisplay','var'), idisplay = 0; end 
 
 if and(length(size(X1))==2,numel(X1)~=9)
-    disp('CMT2omega_xi0.m: input arrays are moment tensors');
+    disp('CMT2omegadc_xi0.m: input arrays are moment tensors');
     M1 = X1; M2 = X2;
     
     % check that M is 6 x n
@@ -46,7 +48,7 @@ if and(length(size(X1))==2,numel(X1)~=9)
     [~,U2] = CMTdecom(M2);
 
 else
-    disp('CMT2omega_xi0.m: input arrays are bases');
+    disp('CMT2omegadc_xi0.m: input arrays are bases');
     U1 = X1; U2 = X2;
     % check dimensions
     [~,~,n1] = size(U1);
@@ -66,12 +68,12 @@ if iorthoU > 0
     U2 = Uorth(U2,iorthoU);
 end
   
-% OMEGA
+% omegadc
 % find angle between closest double couples
 lam0 = repmat([1 0 -1]',1,n);
 MDC1 = CMTrecom(lam0,U1);
 MDC2 = CMTrecom(lam0,U2);
-omega = CMT2omega(MDC1,MDC2);
+omegadc = CMT2omega(MDC1,MDC2);
 
 % XI
 % compute U = U1'*U2
@@ -79,23 +81,23 @@ U = UiU(U1,U2);
 xi0 = U2xi0(U,0,idisplay);
 
 disp(sprintf('%i/%i xi0 values are imaginary',n-sum(~imag(xi0)),n));
-disp(sprintf('%i/%i omega values are imaginary',n-sum(~imag(omega)),n));
+disp(sprintf('%i/%i omegadc values are imaginary',n-sum(~imag(omegadc)),n));
 
 if idisplay==1
-    disp('CMT2omega_xi0.m all omega and xi0:');
+    disp('CMT2omegadc_xi0.m all omegadc and xi0:');
    for ii=1:n
-      disp(sprintf('%12i/%6i: omega = %8.4f, xi0 = %8.4f',ii,n,omega(ii),xi0(ii))); 
+      disp(sprintf('%12i/%6i: omegadc = %8.4f, xi0 = %8.4f',ii,n,omegadc(ii),xi0(ii))); 
    end
 end
 
 if and(bfigure, n>1)
    OMAX = 0.07;  % will depend on bin size
    figure; nr=2; nc=1;
-   subplot(nr,nc,1); hold on; plot_histo(omega,[0:5:180]);
+   subplot(nr,nc,1); hold on; plot_histo(omegadc,[0:5:180]);
    plot([90 90],[0 OMAX],'r','linewidth',2);
    set(gca,'xtick',0:10:180); %axis([0 120 0 OMAX]);
-   xlabel('omega angle describing difference in orientation');
-   title(sprintf('OMEGA: min = %.2f, max = %.2f',min(omega),max(omega)));
+   xlabel('omegadc angle describing difference in orientation');
+   title(sprintf('omegadc: min = %.2f, max = %.2f',min(omegadc),max(omegadc)));
    
    PMAX = 0.12;  % will depend on bin size
    subplot(nr,nc,2); hold on; plot_histo(xi0,[0:5:120]);
@@ -114,7 +116,7 @@ if 0==1
     % see TT2012kaganAppE.m for details
     M1 = [-0.4120    0.0840    0.3280    0.3980   -1.2390    1.0580]'*1e19;
     M2 = [ 5.0540   -2.2350   -2.8190   -0.4760    5.4200    5.5940]'*1e18;
-    [omega,xi0] = CMT2omega_xi0(M1,M2,0,1); omega, xi0
+    [omegadc,xi0] = CMT2omegadc_xi0(M1,M2,0,1); omegadc, xi0
 end
 
 %==========================================================================
