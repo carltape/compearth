04d70ab3ca9fbdce3e386ac9ddf4ded6c1a87d05
Carl Tape <carltape@gmail.com>
2017-01-19 22:20:54 -0900

    set default path to be relative

--- momenttensor/matlab/load_mt.m
+++ momenttensor/matlab/load_mt.m
@@ -3,10 +3,15 @@
 %
 % Set paths to additional directories in compearth
 % (These are needed for utilities such as rotation scripts.)
+%
+% load_mt.m can be called from a matlab startup.m script,
+% so that you never need to execute it.
 
 % path to the compearth head directory 
-%compearth = strcat(getenv('HOME'),'/compearth/');  % absolute path (safest)
-compearth = '../../';                               % relative path
+% it is safest to specify an absolute path to your compearth directory
+%compearth = strcat(getenv('HOME'),'/REPOSITORIES/compearth/');
+% this relative path will only work if you run Matlab from compearth/momenttensor/matlab
+compearth = '../../';
 
 addpath(strcat(compearth,'surfacevel2strain/matlab/util_basic'));
 addpath(strcat(compearth,'surfacevel2strain/matlab/util_euler'));
