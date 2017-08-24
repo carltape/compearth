e8441a8a28eabd2d976495e681167839a51bb212
Ben <bakerb845@gmail.com>
2017-08-24 10:21:32 -0700

    improving documentation

--- momenttensor/c_src/gamma2v.c
+++ momenttensor/c_src/gamma2v.c
@@ -4,15 +4,17 @@
 #include "compearth.h"
 
 /*!
- * @brief Computes v from lune longitude gamma as in Equation 24b of 
- *        Tape and Tape 2015.
+ * @brief Computes v from lune longitude \f$ \gamma \f$ as in
+ *        Equation 24b of Tape and Tape 2015.
  *
- * @param[in] n         number of points in arrays
- * @param[in] gamma     lune longitude (radians) [n].
- *                      note that \f$ \gamma \in [-\pi/6, \pi/6] \f$. 
+ * @param[in] n       Number of points in arrays.
+ * @param[in] gamma   Lune longitudes (radians) such that
+ *                      \f$ \gamma \in [-\pi/6, \pi/6] \f$. 
+ *                    This is an array of dimension [n].
  *
- * @param[out] v        v of equation 24b [n].  note that
+ * @param[out] v      v of equation 24b such that
  *                      \f$ v \in [-1/3,1/3] \f$.
+ *                    This is an array of dimension [n].
  *
  * @date 2016 - Ben Baker converted Carl Tape's gamma2v.m to C
  *
