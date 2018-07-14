e491ed1c237403a60fe780e6303d1ff08cc358da
Carl Tape <carltape@gmail.com>
2018-07-13 16:39:09 -0800

    comments about basis for TT2015AppA

--- momenttensor/matlab/TT2015AppA.m
+++ momenttensor/matlab/TT2015AppA.m
@@ -8,10 +8,10 @@
 %
 % It uses several functions stored within the github repository 'compearth'.
 %
-% Carl Tape, 7/14/2015
+% Carl Tape, 2015-07-14
 %
 
-clear, clc, close all
+clear, close all
 
 deg = 180/pi;
 
@@ -59,6 +59,9 @@ else        % OPTION 2: implement equations from TapeTape2015 directly
     U = V*Yrot;
     
     % Eq 4
+    % note: M is a matrix of numbers, NOT a moment tensor with an assumed basis.
+    %       But if you want to recover the input values of kappa, sigma,
+    %       and theta from M, then M needs to be assumed to have a basis of north-west-up.
     M = U * diag(Lambda) * inv(U);
 end
 
@@ -67,6 +70,27 @@ disp('compare output with TapeTape2015, Appendix A:');
 disp(sprintf('(beta, gamma) = (%.3f, %.3f) = (%.1f deg, %.1f deg)',beta/deg, gamma/deg, beta, gamma));
 disp(sprintf('Lambda = (%.3f, %.3f, %.3f)',Lambda));
 disp(sprintf('theta = %.3f = %.3f deg',theta/deg,theta));
+disp('note: the basis for U and M is north-west-up');
 U, M
+
+%--------------------------------------------------------------------------
+% Note about moment tensor basis
+% The choice of basis will matter when checking the entries in the moment
+% tensor. The example calculations in TT2015 and in TT2012 use north-west-up.
+% This is not stated explicitly in TT2015 Appendix A, but here are some passages that do:
+%   TT2015, Section S4.2: "(The vector e1 is assumed north and e3 is up.)"
+%   TT2015, Figure S3 caption: "If the vector e1 points north and e3 points up,
+%           then κ, σ, θ are the strike, slip, and dip angles for
+%           one of the two fault planes of [Λ]_VY−π/4 ."
+%   TT2012, p. 10: "...where the standard basis is e1 (north), e2 (west), e3 (zenith)"
+%   TT2012, Figure 14.
+% see convert_MT.m for referenced publications that use various basis conventions
+
+% comment this break out to run the rest
+break
+
+disp('moment tensor in south-east-up (GCMT convention):');
+Mseu = convert_MT(3,1,Mvec2Mmat(M,0));
+Mvec2Mmat(Mseu,1)
 
 %==========================================================================
